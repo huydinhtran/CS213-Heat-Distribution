9750665912bb3c81004eb4a3e457d5d858a28f24
Huy Tran <trandinhhuy99@gmail.com>
2022-11-04 15:44:36 -0700

    update

---
+++ compile.sh
@@ -0,0 +1,2 @@
+gcc -fdiagnostics-color=always -g -O4 heatseq.c -o heatseq -fopenmp
+#gcc -fdiagnostics-color=always -g -O4 heatpar.c -o heatpar -fopenmp
---
+++ heatseq.c
@@ -0,0 +1,58 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    argc == 3;
+
+    double start, end; // used for timing
+
+    int n_size, input_iter, iter, i, j;
+    n_size = atoi(argv[1]);
+    input_iter = atoi(argv[2]);
+
+    printf("N size: %d\n",n_size);
+    printf("Iterations: %d\n",input_iter);
+
+    double ***h = (double***)malloc((n_size+1)*sizeof(double**));
+    
+    for (i = 0; i < (n_size+1); i++) {
+        h[i] = (double**)malloc((n_size+1)*sizeof(double*));
+        for (j = 0; j < (n_size+1); j++) {
+            h[i][j] = (double*)malloc(2*sizeof(double));
+        }
+    }
+
+    //initializing
+    for(i = 0; i < n_size+1; i++){
+        for(j = 0; j < n_size+1; j++){
+            h[i][j][0] = 20.0;
+            h[i][j][1] = 20.0;
+        }
+    }
+
+    for(j = 0; j < n_size; j++){
+        if(  (j >= (n_size * 0.3)) && (j < (n_size * 0.7))  ){
+            h[0][j][0] = 100.0;
+            h[0][j][1] = 100.0;
+        }
+    }
+
+    //algorithm
+    for (iter = 0; iter < input_iter; iter++){
+        for(i = 1; i < n_size-1; i++){
+            for(j = 1; j < n_size-1; j++){
+                h[i][j][(iter+1)%2] = 0.25 * (  h[i-1][j][iter%2] + h[i+1][j][iter%2] + h[i][j-1][iter%2] + h[i][j+1][iter%2]  );
+            }
+        }
+    }
+
+    for(i = 0; i < n_size; i++){
+        for(j = 0; j < n_size; j++){
+            printf("%f ", h[i][j][(input_iter+1)%2]);
+        }
+        printf("\n");
+    }
+
+    return(0);
+}
